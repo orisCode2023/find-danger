b81268cd1f6664291cd1b3b341d3f85bf9282dc7
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-17 12:15:35 +0200

    searching by value

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-import { error } from 'node:console'
-import fs from 'node:fs'
-import path from 'node:path'
-
-// const peopleData = {
-//     personName,
-//     age, 
-//     proffesion,
-//     numberOfChildren, 
-//     hobbies,
-//     favoriteFood
-// }
-
-// const transcriptions = {
-//     id, 
-//     callContant, 
-//     age
-// }  
+// import input from 'analiza-sync'
+import fs from 'fs/promises'
 
 function writeToJson(fileName, data){
     fs.writeFile(fileName, convertToJson(data), 'utf-8', (error) => error ? console.log(error): console.log('JSON file created successfully!'))
@@ -25,19 +9,31 @@ function convertToJson(data){
     return JSON.stringify(data, null, 2)
 }
 
-function readFromJson(fileName){
-    fs.readFile(fileName, 'utf-8', (error, data) => error ? error: console.log("data: ", data))
+async function readFromJson(fileName){
+    return JSON.parse(await fs.readFile(fileName, 'utf-8'))
 }
 
 function getPath(url ,param){
     return`${url}${param.toUpperCase()}.json`
 }
 
-async function getByParam(jsonUrl, param){
-    const path = getPath(jsonUrl, param)
-    const result = fetch(`https://spies-test-server-pink.vercel.app/${param}`)
+async function getByParam(jsonPath, param){
+    const result = await fetch(`https://spies-test-server-pink.vercel.app/${param}`)
     console.log(result)
-    writeToJson(fileName, result)
+    const path = getPath(jsonPath, param)
+    writeToJson(path, await result.json())
 }
-
+function printObject(array){
+    array.forEach((value) => {
+        console.log(value)
+    })
+}
+async function searchByParam(fileName, prototype, value){
+    let data = await readFromJson(fileName)
+    let obj = data.filter((object) => object[prototype] === value)
+    if (obj.length === 0){
+        console.log(`${value} was not found`)
+    } printObject(obj)
+}
+searchByParam("./PEOPLE.json", "name", "Shira")
 
