845516147916fd3ac72966af308da517e05ea4e1
odmark2023@gmail.com <odmark2023@gmail.com>
2025-12-17 10:59:39 +0200

    function to get the file and write to it using params

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { error } from 'node:console'
 import fs from 'node:fs'
+import path from 'node:path'
 
 // const peopleData = {
 //     personName,
@@ -17,7 +18,7 @@ import fs from 'node:fs'
 // }  
 
 function writeToJson(fileName, data){
-    fs.writeFile(fileName, data, 'utf-8', (error) => error ? console.log(error): console.log('JSON file created successfully!'))
+    fs.writeFile(fileName, convertToJson(data), 'utf-8', (error) => error ? console.log(error): console.log('JSON file created successfully!'))
 }
 
 function convertToJson(data){
@@ -27,4 +28,16 @@ function convertToJson(data){
 function readFromJson(fileName){
     fs.readFile(fileName, 'utf-8', (error, data) => error ? error: console.log("data: ", data))
 }
+
+function getPath(url ,param){
+    return`${url}${param.toUpperCase()}.json`
+}
+
+async function getByParam(jsonUrl, param){
+    const path = getPath(jsonUrl, param)
+    const result = fetch(`https://spies-test-server-pink.vercel.app/${param}`)
+    console.log(result)
+    writeToJson(fileName, result)
+}
+
 
